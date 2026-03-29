6efa3efbeaa23e1d0958772196e6d099c8a49f01
yatofrmdaowic-netizen <yatofrmdaowic@gmail.com>
2026-03-29 18:25:29 +0530

    Add exchange configuration data

---
+++ exchange.js
@@ -0,0 +1,17 @@
+[
+  {
+    "id": "small",
+    "limit": 50,
+    "coin": 500
+  },
+  {
+    "id": "medium",
+    "limit": 100,
+    "coin": 1200
+  },
+  {
+    "id": "large",
+    "limit": 200,
+    "coin": 3000
+  }
+]
